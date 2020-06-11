eb15023d4c356c00055ea3e671b6f01868c178fc
Sergbab86 <66358305+Sergbab86@users.noreply.github.com>
2020-06-11 10:40:06 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+
+print('Escape sequences \
+        \n\\a Bell (alert) \
+        \n\\b Backspace \
+        \n\\n New line \
+        \n\\t Horizontal tab \
+        \n\\\ Backslash \\ \
+        \n\\\" Double quotation mark \" ')
+print("\\' Single quotation mark '")
