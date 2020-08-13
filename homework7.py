1781e0ef53b59ac986b6d40d481fbe27d38d6f57
Sergbab86 <Qazx1324!>
2020-08-13 20:01:27 +0300

    done

---
+++ homework7.py
@@ -0,0 +1,11 @@
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x1 - y1) == 1 and (x2 - y2) == 2:
+    print('Да')
+elif (x1 -y1) == 2 and (x2 - y2) == 1:
+    print('Да')
+else:
+    print('Нет')
