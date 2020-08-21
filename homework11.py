2e847622b5aeecf255ff210872d24bc268d9b777
Sergbab86 <Qazx1324!>
2020-08-21 11:49:47 +0300

    Done

---
+++ homework11.py
@@ -0,0 +1,57 @@
+
+h = 13
+
+for i in range(h // 2 + 1):
+    for j in range(h):
+        if (
+                i == h // 2
+                or j == h // 2 + i
+                or i == h // 2 - j
+        ):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+for i in range(h // 2 + 1):
+    for j in range(h):
+        if (
+                i == h // 2
+                or (j <= h //2 + i and i >= h // 2 - j)
+        ):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+
+for i in range(h):
+    for j in range(h):
+        if (
+                i == h // 2
+                or (j <= h //2 + i and i >= h // 2 - j and i < h // 2)
+                or (j == i - h // 2 and i > h // 2)
+                or (j == h - i + (h // 2 - 1) and i > h // 2)
+        ):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
+
+for i in range(h):
+    for j in range(h):
+        if (
+                i == h // 2
+                or (j <= h //2 + i and i >= h // 2 - j and i < h // 2)
+                or (j == i - h // 2 and i > h // 2)
+                or (j == h - i + (h // 2 - 1) and i > h // 2)
+                or j == h // 2
+        ):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
